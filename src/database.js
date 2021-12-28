b3362203ff52b5f06fbbb7aee40ae1f85a032b64
José Miranda <josemiranda989@gmail.com>
2021-12-28 15:02:22 -0300

    uri direct

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ import { MONGODB_URI } from "./config";
 // connection to db
 (async () => {
   try {
-    const db = await connect(MONGODB_URI);
+    const db = await connect("mongodb+srv://josemiranda989:mongodb1234@cluster0.phrow.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
     console.log("Db connectect to", db.connection.name);
   } catch (error) {
     console.error(error);
